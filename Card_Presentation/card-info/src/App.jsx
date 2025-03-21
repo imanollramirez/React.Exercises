200397036cda3ad8bab36dbd7dd77c468cda9476
imanollramirez <aleimanolramirez@gmail.com>
2025-03-21 15:06:06 -0600

    Card-Info

---
+++ Card_Presentation/card-info/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Card from './components/card.jsx'
+
+function App() {
+
+  return (
+    <>
+      <Card
+      Name= {"Imanol Ramírez"}
+      Occupation={"Student"}
+      Description={"I am guy who fell in love with computer's world. I Study at Ricaldone ITR, I'm 18 years old, i like cars and girls."}
+      />
+    </>
+  )
+}
+
+export default App;
