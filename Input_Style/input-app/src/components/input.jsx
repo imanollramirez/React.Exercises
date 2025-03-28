92f6eadca95c0f7b964a07d2db6aec235876a848
imanollramirez <aleimanolramirez@gmail.com>
2025-03-28 14:56:30 -0600

    changes ..

--- Input_Style/input-app/src/components/input.jsx
+++ Input_Style/input-app/src/components/input.jsx
@@ -1,12 +1,24 @@
+import React, { useState } from 'react';
 import './styles.css'
 
 const Input = () => {
+
+    const [num1, setNum1] = useState('');
+    const [num2, setNum2] = useState('');
+    const [resultado, setResultado] = useState(null);
+
+    const handleSumar = ( ) => {
+        const suma = parseFloat(num1) + parseFloat(num2);
+        setResultado(suma)
+    }
+
     return(
         <>
         <h1 className="title">Ingrese datos</h1>
-		<input placeholder="Number1" className="input" name="text" type="text"/>
-		<input placeholder="Number2" className="input" name="text" type="text"/><br />
-        <button className='btn'>Calcular</button>
+		<input placeholder="Number1" className="input" value={num1} type="number" onChange={(e) => setNum1(e.target.value)}/>
+		<input placeholder="Number2" className="input" value={num2} type="number" onChange={(e) => setNum2(e.target.value)}/><br />
+        <button className='btn' onClick={handleSumar}>Sumar</button>
+        <h2 className="resultado">Resultado: {resultado !== null && <p>Resultado: {resultado}</p>} </h2>
         </>
     )
 };
